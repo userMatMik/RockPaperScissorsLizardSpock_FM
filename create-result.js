956d0df319dd57611c5de993f1100bf4dd41925a
userMatMik <106982809+userMatMik@users.noreply.github.com>
2022-11-08 23:48:59 +0100

    updated styles and dom functions

--- create-result.js
+++ create-result.js
@@ -2,14 +2,17 @@ import { RPSstate } from "./game-logic.js";
 
 const createResultAi = (str) => {
     const resultContainer = document.createElement('div');
-    resultContainer.classList.add('result');
+    resultContainer.classList.add('result-container');
 
     const resultTitleElement = document.createElement('p');
-    resultTitleElement.classList.add('result__title');
+    resultTitleElement.classList.add('result-title');
     resultTitleElement.innerText = str;
 
-    const resultImgContainerElement = document.createElement('div');
-    resultImgContainerElement.classList.add('rersult__img-container');
+    const resultElement = document.createElement('div');
+    resultElement.classList.add('result');
+
+    // const resultImgContainerElement = document.createElement('div');
+    // resultImgContainerElement.classList.add('rersult__img-container', 'result__img-placeholder');
 
     // const imgElement = document.createElement('img');
     // imgElement.setAttribute('src', `./images/icon-${RPSstate.AIPick}.svg`);
@@ -17,29 +20,34 @@ const createResultAi = (str) => {
     // resultImgContainerElement.appendChild(imgElement)
 
     resultContainer.appendChild(resultTitleElement);
-    resultContainer.appendChild(resultImgContainerElement);
+    resultContainer.appendChild(resultElement);
 
     return resultContainer;
 }
 
 const createResult = (str) => {
     const resultContainer = document.createElement('div');
-    resultContainer.classList.add('result');
+    resultContainer.classList.add('result-container');
 
     const resultTitleElement = document.createElement('p');
-    resultTitleElement.classList.add('result__title');
+    resultTitleElement.classList.add('result-title');
     resultTitleElement.innerText = str;
 
+    const resultElement = document.createElement('div');
+    resultElement.classList.add('result');
+
     const resultImgContainerElement = document.createElement('div');
-    resultImgContainerElement.classList.add('rersult__img-container');
+    resultImgContainerElement.classList.add('result__img-container');
 
     const imgElement = document.createElement('img');
     imgElement.setAttribute('src', `./images/icon-${RPSstate.playerPick}.svg`);
 
     resultImgContainerElement.appendChild(imgElement)
+    
+    resultElement.appendChild(resultImgContainerElement);
 
     resultContainer.appendChild(resultTitleElement);
-    resultContainer.appendChild(resultImgContainerElement);
+    resultContainer.appendChild(resultElement);
 
     return resultContainer;
 }
